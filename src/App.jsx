942dd1cd42674d11ad31ba966380a4223f073ad3
SantiagoEsc1216 <santiagoesc1216@gmail.com>
2025-02-18 09:27:20 -0600

    instalación de axios y test

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [mensaje, setMensaje] = useState("");
+
+  useEffect(() => {
+    axios.get("http://127.0.0.1:8000/api/mensaje")
+      .then((response) => {
+        setMensaje(response.data.mensaje);
+      })
+      .catch((error) => {
+        console.error("Error al obtener los datos", error);
+      });
+  }, []);
 
   return (
-    <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
-  )
+    <div className="min-h-screen flex items-center justify-center bg-blue-500 text-white text-3xl">
+      {mensaje || "Cargando..."}
+    </div>
+  );
 }
 
-export default App
+export default App;
