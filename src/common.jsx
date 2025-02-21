64276362907d930f65b35430185a18ae5d3ce97e
SantiagoEsc1216 <santiagoesc1216@gmail.com>
2025-02-21 17:24:47 -0600

    diseño login

---
+++ src/common.jsx
@@ -0,0 +1,6 @@
+import React from "react"
+export function AppHeader() {
+    return (
+        <div>header</div>
+    );
+}
---
+++ src/login.jsx
@@ -0,0 +1,61 @@
+// src/pages/Login.jsx
+import React, { useState } from "react";
+import caceiLogo from './assets/caceiLogo.png'
+import './app.css'
+
+const Login = () => {
+    const [usuario, setUsuario] = useState("");
+    const [contrasena, setContrasena] = useState("");
+
+    const manejarLogin = (e) => {
+        e.preventDefault();
+        // Ejemplo simple de autenticación
+        if (usuario === "admin" && contrasena === "1234") {
+        } else {
+            alert("Usuario o contraseña incorrectos");
+        }
+    };
+
+    return (
+        <>
+            <div className="flex justify-center w-full items-start">
+                <div className=" p-10 w-5/9" id="info">
+                    <h1 class=" font-bold text-3xl">Bienvenido</h1>
+                    <p class="mt-10 mb-20">Este sistema permitirá a los usuarios cargar evidencias en forma de archivos, los cuales serán almacenados en una base de datos y organizados según las categorías y secciones del Marco de Referencia 2025</p>
+                    <img src={caceiLogo} alt="Logo no encontrado" />
+                </div>
+                <div className="flex justify-center items-center h-fit w-3/9">
+                    <form className="p-2 bg-white w-full" onSubmit={manejarLogin}>
+                        <h2 className="text-2xl mb-4 text-center">Ingresar al sistema</h2>
+                        <input
+                            type="text"
+                            placeholder="RPE"
+                            className="w-full mb-4 p-2 border bg-neutral-200"
+                            value={usuario}
+                            onChange={(e) => setUsuario(e.target.value)}
+                            required
+                        />
+                        <input
+                            type="password"
+                            placeholder="Contraseña"
+                            className="w-full mb-4 p-2 border bg-neutral-200"
+                            value={contrasena}
+                            onChange={(e) => setContrasena(e.target.value)}
+                            required
+                        />
+                        <button
+                            type="submit"
+                            className="bg-cyan-500 text-white w-full p-2 rounded hover:bg-cyan-800"
+                        >
+                            Ingresar
+                        </button>
+                    </form>
+                </div>
+            </div>
+
+        </>
+
+    );
+};
+
+export default Login;
