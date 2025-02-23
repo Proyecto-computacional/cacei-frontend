814e10a9d8882aab7160b4974bcb7e670a08875e
SantiagoEsc1216 <santiagoesc1216@gmail.com>
2025-02-23 17:43:09 -0600

    correccion colores header y footer

--- src/common.jsx
+++ src/common.jsx
@@ -6,16 +6,19 @@ export function AppHeader() {
     return (
         <header className="flex h-[25vh] bg-neutral-100 flex-wrap mb-2 select-none">
             <div className="flex flex-nowrap items-center justify-around h-8/10 w-screen">
-                <div className="min-w-2/4 max-w-2/4 h-full bg-blue-900 flex items-center justify-around">
+                <div className="min-w-2/4 max-w-2/4 h-full bg-primary1 flex items-center justify-around">
                     <img src={headerLogo} alt="" className="w-8/10 h-9/10" />
                 </div>
-                <div className="bg-cyan-500 w-1/4 h-full text-neutral-50 
+                <div className="bg-secondary1 w-1/4 h-full text-neutral-50 
                 text-5xl font-light flex items-center text-center justify-center"><p className="w-1/2">Portal CACEI</p></div>
                 <img src={headerImg} alt="" className="w-1/4 h-full" />
             </div>
-            <div className="h-2/10 w-screen mt-3 flex bg-yellow-500">
-                <div className="h-full min-w-1/2 bg-blue-900 clip-triangle"></div>
-                <div className="h-full min-w-1/2 bg-yellow-500 text-gray-950 text-end text-sm pr-1"> "MODOS ET CUNCTARUM RERUM MENSURAS AUDEBO" </div>
+            <div className="h-2/10 w-screen mt-3 flex bg-alt1 items-center">
+                <div className="h-full min-w-1/2 bg-primary1 clip-triangle"></div>
+                <div className="h-full min-w-1/2 bg-alt1 
+                text-gray-950 pr-2 mx-auto flex items-center">
+                    <p className="w-full text-end text-sm">"MODOS ET CUNCTARUM RERUM MENSURAS AUDEBO"</p>
+                </div>
             </div>
         </header>
     );
@@ -23,6 +26,6 @@ export function AppHeader() {
 
 export function AppFooter() {
     return (
-        <div className="min-w-full max-w-full min-h-[15vh] bg-linear-to-t from-blue-900 from-80%  to-cyan-500 to-20%"></div>
+        <div className="min-w-full max-w-full min-h-[15vh] bg-linear-to-t from-primary1 from-80%  to-secondary1 to-20%"></div>
     );
 }
