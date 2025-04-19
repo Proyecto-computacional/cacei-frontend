c6cbf04d5ad112879af3c43868b7b7be230621a8
santiagoesc1216 <santiagoesc1216@gmail.com>
2025-04-19 11:52:24 -0600

    Asigaciones y revisiones

--- src/pages/uploadEvidence.jsx
+++ src/pages/uploadEvidence.jsx
@@ -4,6 +4,8 @@ import FeedbackModal from "../components/Feedback";
 import CriteriaGuide from "../components/CriteriaGuide";
 import '../app.css';
 import api from "../services/api";
+import { FileQuestion } from "lucide-react";
+import Feedback from "../components/Feedback";
 
 const UploadEvidence = () => {
   const [file, setFile] = useState(null);
@@ -54,17 +56,63 @@ const UploadEvidence = () => {
   const [showFeedback, setShowFeedback] = useState(false);
   const [showCriteriaGuide, setShowCriteriaGuide] = useState(false);
 
+  const asignaciones = [
+    {criterio:"Criterio 1.1", estado: "NO CARGADA"},
+    {criterio:"Criterio 1.2", estado: "PENDIENTE"},
+    {criterio:"Criterio 1.3", estado: "NO APROBADA"},
+    {criterio:"Criterio 1.4", estado: "APROBADA"}
+  ]
+
+  const statuses = [
+    {revisor:"Cesar Auguto", estado: "APROBADA", feedback:"feedback"},
+    {revisor:"Silvia Vaca", estado: "NO APROBADA", feedback:"feedback"},
+    {revisor:"Lilia del Carmen", estado: "PENDIENTE", feedback:"feedback"},
+  ]
+
+  const getEstadoClass = (estado) => {
+    switch (estado) {
+      case "NO CARGADA":
+        return "bg-neutral-500 text-neutral-50";
+      case "PENDIENTE":
+        return "bg-yellow-400 text-white";
+      case "NO APROBADA":
+        return "bg-red-500 text-white";
+      case "APROBADA":
+        return "bg-green-500 text-white";
+      default:
+        return "bg-gray-300 text-black";
+    }
+  };
+
   return (
     <>
       <AppHeader />
       <SubHeading />
-      <div className="min-h-screen p-10 flex flex-col items-center relative" style={{ background: "linear-gradient(180deg, #e1e5eb 0%, #FFF 50%)" }}>
-        <h1 className="text-[34px] font-semibold text-black font-['Open_Sans'] mt-2 mb-2 self-start">
-          Subir Evidencia
-        </h1>
-        <div className="bg-white p-6 rounded-lg shadow-lg flex flex-row w-[80%] min-w-7xl min-h-[500px]">
-          <div className="flex flex-col flex-1 mr-10">
-            <p className="text-black text-lg font-semibold">Nombre del criterio</p>
+      <div className="h-fit p-10 flex justify-around items-stretch relative" style={{ background: "linear-gradient(180deg, #e1e5eb 0%, #FFF 50%)" }}>
+        <div className="bg-cyan-300 w-2/10">
+        <p className="w-full text-center text-3xl bg-cyan-500 text-amber-50 py-2">Mis asignaciones</p>
+          <div>
+      {asignaciones.map((item, index) => (
+        <div
+          key={index}
+          className="flex justify-around text-[20px] items-center p-2 cursor-pointer hover:bg-cyan-500">
+          <p className="w-1/2">{item.criterio}</p>
+          <p className={`w-1/2 font-semibold px-3 text-center rounded-lg ${getEstadoClass(item.estado)}`}>
+            {item.estado}
+          </p>
+        </div>
+      ))}
+        </div>
+        </div>
+        <div className="bg-white p-6 rounded-lg shadow-lg flex flex-wrap flex-row w-7/10 min-h-[500px]">
+          <div className="flex flex-col flex-1 mr-10 w-1/2">
+            <h1 className="text-[40px] font-semibold text-black font-['Open_Sans'] mt-2 self-start">
+            Subir Evidencia
+            </h1>
+            <h1 className="text-[25px] font-light text-black font-['Open_Sans'] mb-4 self-start">
+            Sección/categoria/criterio
+            </h1>
+            <p className="text-black text-lg font-semibold">Justificación</p>
             <textarea
               className="w-full p-2 border rounded mt-2 text-gray-600 bg-gray-100 min-h-[150px]"
               placeholder="Descripción"
@@ -90,32 +138,24 @@ const UploadEvidence = () => {
               Retroalimentación
             </button>
           </div>
-          <div className="flex-1 flex justify-center items-center border rounded bg-gray-100 p-4">
-            {file ? (
-              file.name.endsWith(".png") ||
-              file.name.endsWith(".jpg") ||
-              file.name.endsWith(".jpeg") ||
-              file.name.endsWith(".gif") ? (
-                <img src={file.preview} alt="Preview" className="w-full h-full object-cover" />
-              ) : file.name.endsWith(".pdf") ? (
-                <iframe src={file.preview} title="Vista previa PDF" className="w-full h-full" />
-              ) : file.name.endsWith(".docx") || file.name.endsWith(".doc") ? (
-                <p className="text-blue-600 text-center">
-                  Archivo de Word cargado correctamente, pero no se puede previsualizar.
-                </p>
-              ) : file.name.endsWith(".xlsx") ? (
-                <p className="text-gray-600 text-center">
-                  Archivo .xlsx aceptado, pero no se puede previsualizar.
-                </p>
-              ) : (
-                <p className="text-red-600 text-center">
-                  Formato de archivo no permitido.
-                </p>
-              )
-            ) : (
-              <p className="text-gray-600">Preview</p> // Solo muestra esto si no hay archivo
-            )}
+          <div className="w-1/2">
+          <h1 className="text-[40px] font-semibold text-black font-['Open_Sans'] mt-2 self-start">
+            Revisión
+            </h1>
+                {statuses.map((item, index) => (
+                  <div>
+                    <div className="flex">
+                    <p className="text-black text-lg font-semibold">Revisor:</p><p className="text-black text-lg ml-1">{item.revisor}</p>
+                    </div>
+                    <p className="text-black text-lg font-semibold">Estado</p>
+                    <p className={`w-1/2 font-semibold px-3 text-center rounded-lg ${getEstadoClass(item.estado)}`}>{item.estado}</p>
+                    <p className="text-black text-lg font-semibold">Retroalimentación</p>
+                    <p className="w-full p-2 border rounded mt-2 text-gray-600 bg-gray-100 min-h-[150px]">{item.feedback}</p>
+                  </div>
+                ))}
+
           </div>
+          <div className="w-full flex justify-end mt-7"><FileQuestion size={50} onClick={() => {setShowCriteriaGuide(true)}}/></div>
         </div>
       </div>
       <AppFooter />
