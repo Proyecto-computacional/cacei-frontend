64bd9cc72f5952a77b246f9f7844deda299d90d7
BRYANAARONEDO2 <124536363+BRYANAARONEDO2@users.noreply.github.com>
2025-05-02 05:44:58 -0600

    corregi formato de diseño

--- src/common.jsx
+++ src/common.jsx
@@ -7,32 +7,20 @@ import Logout from "./components/logout";
 import NotificationsTable from "./components/NotificationTable";
 import { Mail, Bell, User, Menu } from "lucide-react";
 import { useLocation } from "react-router-dom";
+import './index.css'
+import './app.css'
 
 export function AppHeader() {
     return (
-        <header className="flex h-[25vh] bg-neutral-100 flex-wrap mb-2 select-none">
-            <div className="flex flex-nowrap items-center justify-around h-8/10 w-screen">
-                <div className="min-w-2/4 max-w-2/4 h-full bg-primary1 flex items-center justify-around">
-                    <img src={headerLogo} alt="Logo" className="w-8/10 h-9/10" />
-                </div>
-                <div className="bg-secondary1 w-1/4 h-full text-neutral-50 text-5xl font-light flex items-center text-center justify-center">
-                    <p className="w-1/2">Portal CACEI</p>
-                </div>
-                <img src={headerImg} alt="Header" className="w-1/4 h-full" />
-            </div>
-            <div className="h-2/10 w-screen mt-3 flex bg-alt1 items-center">
-                <div className="h-full min-w-1/2 bg-primary1 clip-triangle"></div>
-                <div className="h-full min-w-1/2 bg-alt1 text-gray-950 pr-2 mx-auto flex items-center">
-                    <p className="w-full text-end text-sm">"MODOS ET CUNCTARUM RERUM MENSURAS AUDEBO"</p>
-                </div>
-            </div>
+        <header>
+            
         </header>
     );
 }
 
 export function AppFooter() {
     return (
-        <div className="min-w-full max-w-full min-h-[15vh] bg-gradient-to-t from-primary1 from-80% to-secondary1 to-20%"></div>
+        <div></div>
     );
 }
 
