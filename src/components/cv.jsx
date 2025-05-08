fdafedd65e7fcb44ecaac0d388585f89539401d9
Mariana Pérez <marianasps003@gmail.com>
2025-05-08 14:40:01 -0600

    an empty row can be deleted on cv

--- src/components/cv.jsx
+++ src/components/cv.jsx
@@ -328,6 +328,17 @@ const CV = () => {
         }));
     };
 
+    const removeRow = (sectionId, rowId) => {
+        setData((prev) => ({
+            ...prev,
+            [sectionId]: prev[sectionId].filter((row) => row.id !== rowId),
+        }));
+    };
+
+    const isRowEmpty = (row) => {
+        return Object.values(row.values).every(value => !value);
+    };
+
     const updateRow = (sectionId, rowId, field, value) => {
         setData((prev) => ({
             ...prev,
@@ -395,6 +406,19 @@ const CV = () => {
                                                         )}
                                                     </td>
                                                 ))}
+                                                <td className="border px-4 py-2">
+                                                    {isRowEmpty(row) && (
+                                                        <button
+                                                            onClick={() => removeRow(section.id, row.id)}
+                                                            className="text-red-500 hover:text-red-700"
+                                                            title="Eliminar fila vacía"
+                                                        >
+                                                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                                                <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
+                                                            </svg>
+                                                        </button>
+                                                    )}
+                                                </td>
                                             </tr>
                                         ))}
                                     </tbody>
