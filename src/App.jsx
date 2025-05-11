910ef4767040e131a1a4117d869cc5f6eeae7665
SantiagoEsc1216 <santiagoesc1216@gmail.com>
2025-05-11 11:35:29 -0600

    rutas protegidas

--- src/App.jsx
+++ src/App.jsx
@@ -24,24 +24,69 @@ function App() {
           </ProtectedRoute>
         } />
         <Route path="/ReviewEvidence" element={
-          <ProtectedRoute>
+          <ProtectedRoute allowedRoles={["ADMINISTRADOR",
+            "COORDINADOR",
+            "JEFE DE ÁREA"]}>
             <ReviewEvidence />
           </ProtectedRoute>
         } />
-        <Route path="/PersonalConfig" element={<PersonalConfig />} />
-        <Route path="/EvidencesCompilation" element={<EvidencesCompilation />} />
-        <Route path="/dash/:processId" element={<Dashboard />} />
-        <Route path="/notifications" element={<Notification />} />
+        <Route path="/PersonalConfig" element={
+          <ProtectedRoute>
+            <PersonalConfig />
+          </ProtectedRoute>
+        } />
+        <Route path="/personalInfo" element={
+          <ProtectedRoute>
+            <PersonalConfig />
+          </ProtectedRoute>
+        } />
+        <Route path="/EvidencesCompilation" element={
+          <ProtectedRoute allowedRoles={["ADMINISTRADOR"]}>
+            <EvidencesCompilation />
+          </ProtectedRoute>
+        } />
+        <Route path="/dash/:processId" element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        } />
+        <Route path="/notifications" element={
+          <ProtectedRoute>
+            <Notification />
+          </ProtectedRoute>
+        } />
         <Route path="/UsersAdmin" element={
-          <ProtectedRoute allowedRoles={["PROFESOR"]}>
+          <ProtectedRoute allowedRoles={["ADMINISTRADOR"]}>
             <UsersAdmin />
           </ProtectedRoute>
         } />
-        <Route path="/personalInfo" element={<PersonalConfig />} />
-        <Route path="/uploadEvidence/:evidence_id" element={<UploadEvidence />} />
-        <Route path="/evidenceManagement" element={<EvidenceManagement />} />
-        <Route path="/framesAdmin" element={<FrameOfReferenceView/>}/>
-        <Route path="/framesStructure/:frameID" element={<EstructuraMarco/>}/>
+        <Route path="/uploadEvidence" element={
+          <ProtectedRoute>
+            <UploadEvidence />
+          </ProtectedRoute>
+        } />
+        <Route path="/uploadEvidence/:evidence_id" element={
+          <ProtectedRoute>
+            <UploadEvidence />
+          </ProtectedRoute>
+        } />
+        <Route path="/evidenceManagement" element={
+          <ProtectedRoute allowedRoles={["ADMINISTRADOR",
+            "COORDINADOR",
+            "JEFE DE AREA"]}>
+            <EvidenceManagement />
+          </ProtectedRoute>
+        } />
+        <Route path="/framesAdmin" element={
+          <ProtectedRoute allowedRoles={["ADMINISTRADOR"]}>
+            <FrameOfReferenceView />
+          </ProtectedRoute>
+        } />
+        <Route path="/framesStructure/:frameID" element={
+          <ProtectedRoute allowedRoles={["ADMINISTRADOR"]}>
+            <EstructuraMarco />
+          </ProtectedRoute>
+        } />
       </Routes>
     </Router>
   );
