573557f5601dc6c295cbdaeec1363939f5192016
Rodriguez Garcia Juan Pablo <112775638+JuanPablo2003@users.noreply.github.com>
2025-10-20 23:12:11 -0600

    Implementación de búsqueda de procesos
    Hay un cuadro de entrada de texto, donde se le sugiere al usuario escribir el nombre de un área, marco o carrera, y solo le mostrará los resultados relacionados.
    
    La función de eliminar procesos está en avance

--- src/pages/MainMenu.jsx
+++ src/pages/MainMenu.jsx
@@ -3,22 +3,29 @@ import { useLocation, useNavigate } from "react-router-dom";
 import api from "../services/api"
 import { AppHeader, AppFooter, SubHeading } from "../common";
 import Card from "../components/Card";
-import { Plus } from "lucide-react";
+import { Plus, X } from "lucide-react";
 import CreateProcessModal from "../components/CreateProcessModal";
+import DeleteProcessModal from "../components/DeleteProcessModal";
 import LoadingSpinner from "../components/LoadingSpinner";
+import { Search } from "lucide-react";
 
 const MainMenu = () => {
-  const [cards, setCards] = useState([]);
+  const [processes, setProcesses] = useState([]);
   const [percentages, setPercentages] = useState({});
   const [userRole, setUserRole] = useState("");
   const location = useLocation();
   const userRpe = localStorage.getItem('rpe');
   const navigate = useNavigate();
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
   const [finishedStatus, setFinishedStatus] = useState({});
   const [loading, setLoading] = useState(true);
 
-  // Obtener rol de usuario
+  const [selectedArea, setSelectedArea] = useState("-1");
+  const [filteredProcesses, setFilteredProcesses] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [areas, setAreas] = useState([]);
+
   // # Obtener rol de usuario
   useEffect(() => {
     const fetchUserRole = async () => {
@@ -37,32 +44,36 @@ const MainMenu = () => {
   const fetchProcessesData = useCallback(async (processes, role, rpe) => {
     try {
       // Preparar todas las promesas para ejecución en paralelo
-      const promises = processes.map(async (card) => {
+      const promises = processes.map(async (proc) => {
         try {
+          console.log('rpe:', rpe);
+          console.log('proc variable:', proc);            // si renombraste como proc (ver abajo)
+          console.log('frame_name:', proc?.frame_name);
+          console.log('career_name:', proc?.career_name);
           let estPromise;
           let processPromise;
 
           // Configurar llamadas según el rol
           if (role === "ADMINISTRADOR" || role === "JEFE DE AREA" || role === "COORDINADOR DE CARRERA" || role === "DIRECTIVO") {
-            estPromise = api.get(`/estadisticas/${rpe}/${card.frame_name}/${card.career_name}`);
+            estPromise = api.get(`/estadisticas/${rpe}/${proc.frame_name}/${proc.career_name}`);
           } else if (role === "PROFESOR" || role === "DEPARTAMENTO UNIVERSITARIO") {
-            estPromise = api.get(`/estadisticas/por-autor/${rpe}/${card.frame_name}/${card.career_name}`);
+            estPromise = api.get(`/estadisticas/por-autor/${rpe}/${proc.frame_name}/${proc.career_name}`);
           }
 
-          processPromise = api.get(`/api/processes/${card.process_id}/find`);
+          processPromise = api.get(`/api/processes/${proc.process_id}/find`);
 
           // Ejecutar en paralelo
           const [estRes, processRes] = await Promise.allSettled([estPromise, processPromise]);
 
           return {
-            processId: card.process_id,
+            processId: proc.process_id,
             percentage: estRes.status === 'fulfilled' ? (estRes.value?.data[0]?.aprobado ?? 0) : 0,
             finished: processRes.status === 'fulfilled' ? processRes.value.data.finished : false
           };
         } catch (error) {
-          console.warn(`Error obteniendo datos para ${card.process_id}`, error);
+          console.warn(`Error obteniendo datos para ${proc.process_id}`, error);
           return {
-            processId: card.process_id,
+            processId: proc.process_id,
             percentage: 0,
             finished: false
           };
@@ -80,6 +91,37 @@ const MainMenu = () => {
         percentagesMap[result.processId] = result.percentage;
         finishedStatusMap[result.processId] = result.finished;
       });
+
+      try {
+          setLoading(true);
+          const [responseAreas] = await Promise.all([
+          api.get("/api/areas", { // <-- Aquí tu segunda consulta
+              headers: {
+                  Authorization: `Bearer ${localStorage.getItem('token')}`,
+                  "Content-Type": "application/json",
+              },
+          })
+      ]);
+          setAreas(responseAreas.data);
+      
+      } catch (error) {
+          if (error.response) {
+              if (error.response.status === 403 ) {
+                  setModalAlertMessage("No tienes permisos para acceder a esta sección.");
+                  window.location.href = "/PersonalConfig";
+              } else if (error.response.status === 401) {
+                  setModalAlertMessage("Sesión expirada. Inicia sesión de nuevo.");
+                  window.location.href = "/";
+              } else {
+                  setModalAlertMessage("Error desconocido al obtener los usuarios.");
+              }
+          } else {
+              setModalAlertMessage("Error de conexión con el servidor.");
+          }
+          console.error("Error al obtener los usuarios:", error);
+      } finally {
+          setLoading(false);
+      }
 
       return { percentagesMap, finishedStatusMap };
     } catch (error) {
@@ -127,7 +169,8 @@ const MainMenu = () => {
         }
 
         const data = response.data;
-        setCards(data);
+        setProcesses(data);
+        setFilteredProcesses(data)
 
         // Si no hay procesos, terminar aquí
         if (data.length === 0) {
@@ -164,13 +207,44 @@ const MainMenu = () => {
   };
 
   const handleCreateProcess = () => {
-    setIsModalOpen(true);
+    setIsCreateModalOpen(true);
   };
 
-  const handleProcessCreated = () => {
+  const handleDeleteProcess = () => {
+    setIsDeleteModalOpen(true);
+  };
+
+  const handleProcessAlterated = () => {
     window.location.reload();
   };
 
+  // # Funciones para búsqueda de procesos
+  const handleSearch = () => {
+      let filtered = processes;
+
+      // Filtro por área
+      if (selectedArea !== "-1") {
+          filtered = filtered.filter(proc => proc.area_name === selectedArea);
+      }
+
+      // Filtro por término de búsqueda
+      const trimmedSearch = searchTerm.trim().toLowerCase();
+      if (trimmedSearch !== "") {
+          filtered = filtered.filter(proc =>
+              proc.area_name?.toLowerCase().includes(trimmedSearch.toLowerCase()) ||
+              proc.career?.toLowerCase().includes(trimmedSearch.toLowerCase()) ||
+              proc.frame_name?.toLowerCase().includes(trimmedSearch.toLowerCase())
+          );
+      }
+
+      setFilteredProcesses(filtered);
+  };
+
+  useEffect(() => {
+      handleSearch();
+  }, [selectedArea, searchTerm]);
+
+  // # HTML --------------------------------------------------------------------------------------------------------------------------
   return (
     <>
       <AppHeader />
@@ -199,28 +273,58 @@ const MainMenu = () => {
             )}
           </div>
 
+          {/* Búsqueda de procesos */}
+          <div className="flex justify-between items-end mb-8">
+            <div className="relative">
+                <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-5 w-5" />
+                <input
+                    type="text"
+                    placeholder="Buscar por marco, área o carrera..."
+                    className="pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#004A98] focus:border-transparent w-96"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+            </div>
+          </div>
+
+          {/* Lista de procesos (fltrados) */}
           {loading ? (
             <div className="col-span-full flex justify-center py-12">
               <LoadingSpinner />
             </div>
-          ) : cards.length > 0 ? (
+            
+          ) : filteredProcesses.length > 0 ? (
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-              {cards.map((card) => (
+              
+              {filteredProcesses.map((proc) => (
                 <div
-                  key={card.process_id}
+                  key={proc.process_id}
                   className="transform transition-all duration-300 hover:scale-105"
                 >
                   <Card
-                    title={card.frame_name}
-                    area={card.area_name}
-                    career={card.career_name}
-                    percentage={`${percentages[card.process_id] ?? 0}%`}
-                    finished={finishedStatus[card.process_id]}
-                    startDate={card.start_date}
-                    endDate={card.end_date}
-                    dueDate={card.due_date}
-                    onClick={() => handleCardClick(card.process_id, card.frame_name, card.career_name, card.frame_id, finishedStatus[card.process_id])}
+                    title={proc.frame_name}
+                    area={proc.area_name}
+                    career={proc.career_name}
+                    percentage={`${percentages[proc.process_id] ?? 0}%`}
+                    finished={finishedStatus[proc.process_id]}
+                    startDate={proc.start_date}
+                    endDate={proc.end_date}
+                    dueDate={proc.due_date}
+                    onClick={() => handleCardClick(proc.process_id, proc.frame_name, proc.career_name, proc.frame_id, finishedStatus[proc.process_id])}
                   />
+
+                  {userRole === "ADMINISTRADOR" && (
+                  <div className="mt-2 flex justify-end">
+                    <button
+                      onClick={() => handleDeleteProcess(proc.process_id)}
+                      className="bg-red-600 text-white px-4 py-2 rounded-lg hover:bg-red-800 transition-colors duration-300 flex items-center gap-2 shadow-sm hover:shadow-md text-sm"
+                    >
+                      <X className="h-4 w-4" />
+                      Eliminar proceso
+                    </button>
+                  </div>
+                  )}
+
                 </div>
 
               ))}
@@ -241,9 +345,14 @@ const MainMenu = () => {
 
       {/* Modales */}
       <CreateProcessModal
-        isOpen={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
-        onSuccess={handleProcessCreated}
+        isOpen={isCreateModalOpen}
+        onClose={() => setIsCreateModalOpen(false)}
+        onSuccess={handleProcessAlterated}
+      />
+      <DeleteProcessModal
+        isOpen={isDeleteModalOpen}
+        onClose={() => setIsDeleteModalOpen(false)}
+        onSuccess={handleProcessAlterated}
       />
     </>
   );
